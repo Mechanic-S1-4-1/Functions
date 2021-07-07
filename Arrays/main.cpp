ec636827a7d074c118316aa2cc76b48d95794a14
Олег Матющенко <oleg_m1987@outlook.com>
2021-07-07 18:17:22 +0700

    ArrayFunctions

---
+++ Arrays/main.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+
+#define tab "\t"
+
+using namespace std;
+
+void FillRand(int arr[], const int n);
+void Print(int arr[], const int n);
+void PrintReverse(int arr[], const int n);
+int Sum(int arr[], const int n);
+double Avg(int arr[], const int n);
+int minValueIn(int arr[], const int n);
+int maxValueIn(int arr[], const int n);
+
+void main() {
+
+	setlocale(0, "russian");
+
+	const int n = 5;
+	int arr[n] = {3,5,8,9,4};
+	FillRand(arr, n);
+	Print(arr, n);
+	PrintReverse(arr,n);
+	cout << "Сумма элементов массива: " << Sum(arr, n) << endl;
+	cout << "Среднее арифметическое элементов массива: " << Avg(arr, n) << endl;
+	cout << "Минимальное значение в массиве: " << minValueIn(arr, n) << endl;
+	cout << "Максимальное значение в массиве: " << maxValueIn(arr, n) << endl;
+	
+}
+
+void FillRand(int arr[], const int n)
+{
+	for (int i = 0;i < n;i++) {
+		arr[i] = rand()%100; // фукция rand() возвращает псевдослучайное число,
+						 // в промежутке от 0 до 32 767.
+		                 // Если хотим сделать случайное число меньше,
+		                 // то можно например поставить ограничить значением 100, 
+		                 // а именно rand()%100
+	}
+}
+void Print(int arr[], const int n) 
+{
+	for (int i = 0;i < n;i++) 
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+}
+void PrintReverse(int arr[], const int n)
+{
+	for (int i = n-1;i >= 0;i--)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+}
+int Sum(int arr[], const int n)
+{
+	int sum = 0;
+	for (int i = 0;i < n;i++)
+	{
+		sum += arr[i];
+	}
+	return sum;
+}
+double Avg(int arr[], const int n)
+{
+	return (double)Sum(arr,n)/n;
+}
+int minValueIn(int arr[], const int n)
+{
+	int min = arr[0];
+	for (int i = 0;i < n;i++)
+	{
+		if (arr[i] < min)
+			min=arr[i];
+	}
+	return min;
+}
+int maxValueIn(int arr[], const int n)
+{
+	int max = arr[0];
+	for (int i = 0;i < n;i++)
+	{
+		if (arr[i] > max)
+			max = arr[i];
+	}
+	return max;
+}
